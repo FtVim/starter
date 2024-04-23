dff38483b179ff68b4927846b195becb6b475e44
Santito <Santito@noreply>
2024-04-23 18:55:06 +0200

    🚧 Spotted

--- init.lua
+++ init.lua
@@ -10,5 +10,28 @@ require "config.options"
 -- See `./lua/config/keymaps.lua`
 require "config.keymaps"
 
--- See `./lua/lazy.lua`
-require "lazy"
+-- [[ Install `lazy.nvim` plugin manager ]]
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system {
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "--single-branch",
+    "https://github.com/folke/lazy.nvim.git",
+    lazypath,
+  }
+end
+vim.opt.runtimepath:prepend(lazypath)
+
+require("lazy").setup {
+  spec = {
+    -- add FtVim and import its plugins
+    { "FtVim/FtVim", import = "ftvim.plugins" },
+    -- import/override with your plugins
+    { import = "plugins" },
+  },
+
+  install = { colorscheme = { "tokyonight", "habamax" } },
+  checker = { enabled = true, notify = false }, -- automatically check for plugin updates
+}
