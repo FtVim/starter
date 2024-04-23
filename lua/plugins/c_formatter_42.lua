ee693b6086b9b552c073d7f35abbfe776146844b
Santito <Santito@noreply>
2024-04-23 19:25:56 +0200

    🔖 SIII

--- lua/plugins/c_formatter_42.lua
+++
@@ -1,5 +0,0 @@
-return {
-  "cacharle/c_formatter_42.vim",
-  config = function()
-  end,
-}
---
+++ lua/plugins/init.lua
@@ -0,0 +1,10 @@
+return {
+	{
+		"cacharle/c_formatter_42.vim",
+		config = function() end,
+	},
+	{
+		"42Paris/42header",
+		config = function() end,
+	},
+}
