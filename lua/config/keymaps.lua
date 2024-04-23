8b7863b35da0021028dcc33b9143437d3c247cd3
Bautista Rodriguez <bautirodri@gmail.com>
2024-04-23 18:14:52 +0200

    map

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -68,6 +68,12 @@ map("n", "<leader>K", "<cmd>norm! K<cr>", { desc = "Keywordprg" })
 map("v", "<", "<gv")
 map("v", ">", ">gv")
 
+-- learn to move
+map("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
+map("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
+map("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
+map("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
+
 -- lazy
 map("n", "<leader>l", "<cmd>Lazy<cr>", { desc = "Lazy" })
 
--- lua/user/config/init.lua
+++
@@ -1,2 +0,0 @@
-require("user.config.options")
-require("user.config.keymaps")
