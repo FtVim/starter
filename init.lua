1e9114ccb420ba14298cf827c7acdf56847e16b1
Santito <Santito@noreply>
2024-04-23 18:32:45 +0200

    init.lua ⬆️

--- init.lua
+++ init.lua
@@ -1,2 +1,11 @@
-require "config"
+--	    _____________    ________  ___
+--	   / ____/_  __/ |  / /  _/  |/  /
+--	  / /_    / /  | | / // // /|_/ /
+--	 / __/   / /   | |/ // // /  / /
+--	/_/     /_/    |___/___/_/  /_/
+
+-- See `./lua/lazy.lua`
 require "lazy"
+
+-- See `./lua/config/init.lua`
+require "config"
