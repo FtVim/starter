386dbc0bbf186de8e56c7b2516d0ee098f0fd1fd
Santito <68120901+San-tito@users.noreply.github.com>
2024-04-23 20:59:29 +0200

    Update init.lua 📈

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -7,19 +7,4 @@ return {
 		"42Paris/42header",
 		config = function() end,
 	},
-	{
-		"goolord/alpha-nvim",
-		opts = function(_, opts)
-			-- customize the header
-			opts.section.header.val = {
-				"    _____________    ________  ___",
-				"   / ____/_  __/ |  / /  _/  |/  /",
-				"  / /_    / /  | | / // // /|_/ / ",
-				" / __/   / /   | |/ // // /  / /  ",
-				"/_/     /_/    |___/___/_/  /_/   ",
-				"                                   ",
-			}
-			return opts
-		end,
-	},
 }
