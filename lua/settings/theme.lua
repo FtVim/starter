ae9c46091e85bcb7f4d31f5bd1750b9714425150
Bauti <bautirodri@gmal.com>
2024-05-14 23:29:36 +0200

    🔥

--- lua/settings/theme.lua
+++ lua/settings/theme.lua
@@ -1,5 +1,5 @@
 -- Themery block
 -- This block will be replaced by Themery.
-vim.cmd("colorscheme kanagawa-dragon")
-vim.g.theme_id = 4
+vim.cmd("colorscheme oldworld")
+vim.g.theme_id = 9
 -- end themery block
