980fc80ea73e43f157e46dcf616b45e322016465
San-tito <68120901+San-tito@users.noreply.github.com>
2024-06-27 00:37:26 +0200

    feature-updates

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,2 +1,1 @@
-
 -- add yours here
--- lua/options.lua
+++ lua/options.lua
@@ -1,5 +1,1 @@
-ftvim.format_on_save = false
-
-ftvim.builtin.treesitter.highlight.enable = true
-
-ftvim.builtin.treesitter.ensure_installed = { "cpp", "c" }
+-- Add yours
