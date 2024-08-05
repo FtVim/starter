56c3d01efa42af339480655a08530af4b41a7270
Bautista Rodriguez <bautirodri@gmail.com>
2024-08-05 17:41:13 +0200

    Update starter

--- init.lua
+++ init.lua
@@ -1,40 +1,45 @@
---	    _____________    ________  ___
---	   / ____/_  __/ |  / /  _/  |/  /
---	  / /_    / /  | | / // // /|_/ /
---	 / __/   / /   | |/ // // /  / /
---	/_/     /_/    |___/___/_/  /_/
+--          _____________    ________  ___
+--         / ____/_  __/ |  / /  _/  |/  /
+--        / /_    / /  | | / // // /|_/ /
+--       / __/   / /   | |/ // // /  / /
+--      /_/     /_/    |___/___/_/  /_/
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"--single-branch",
-		"https://github.com/folke/lazy.nvim.git",
-		lazypath,
-	})
+        vim.fn.system({
+                "git",
+                "clone",
+                "--filter=blob:none",
+                "--single-branch",
+                "https://github.com/folke/lazy.nvim.git",
+                lazypath,
+        })
 end
+
 vim.opt.runtimepath:prepend(lazypath)
 
 require("lazy").setup({
-	spec = {
-		-- add FtVim and import its plugins
-		{
-			"FtVim/FtVim",
-			lazy = false,
-			branch = "feature-updates",
-			import = "ftvim.plugins",
-			config = function()
-				require("options")
-			end,
-		},
-		-- import/override with your plugins
-		{ import = "plugins" },
-	},
-	checker = { enabled = true, notify = true }, -- automatically check for plugin updates
+        spec = {
+                -- add FtVim and import its plugins
+                {
+                        "FtVim/FtVim",
+                        lazy = false,
+                        branch = "feature-updates",
+                        import = "ftvim.plugins",
+                        config = function()
+                                require("options")
+                        end,
+                },
+                -- import/override with your plugins
+                { import = "plugins" },
+        },
+        checker = { enabled = true, notify = true }, -- automatically check for plugin updates
 })
 
+vim.cmd("colorscheme " .. ftvim.colorscheme)
+
+require("ftvim.utils.modules").learn_to_move()
+
 vim.schedule(function()
-	require("keymaps")
+        require("keymaps")
 end)
