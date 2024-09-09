c29111f317db2e8fe737917bce84251f0bebb9dc
Bautista Rodriguez Ramal <bautrodr@cbr15s4.42barcelona.com>
2024-09-09 15:04:06 +0200

    new-starter

--- init.lua
+++ init.lua
@@ -1,39 +1,51 @@
---	    _____________    ________  ___
---	   / ____/_  __/ |  / /  _/  |/  /
---	  / /_    / /  | | / // // /|_/ /
---	 / __/   / /   | |/ // // /  / /
---	/_/     /_/    |___/___/_/  /_/
+--          _____________    ________  ___
+--         / ____/_  __/ |  / /  _/  |/  /
+--        / /_    / /  | | / // // /|_/ /
+--       / __/   / /   | |/ // // /  / /
+--      /_/     /_/    |___/___/_/  /_/
 
--- See `./lua/config/options.lua`
-require "config.options"
-
--- See `./lua/config/keymaps.lua`
-require "config.keymaps"
--- [[ Install `lazy.nvim` plugin manager ]]
-local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system {
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "--single-branch",
-    "https://github.com/folke/lazy.nvim.git",
-    lazypath,
-  }
+        vim.fn.system({
+                "git",
+                "clone",
+                "--filter=blob:none",
+                "--single-branch",
+                "https://github.com/folke/lazy.nvim.git",
+                lazypath,
+        })
 end
+
 vim.opt.runtimepath:prepend(lazypath)
 
-require("lazy").setup {
-  spec = {
-    -- add FtVim and import its plugins
-    { "FtVim/FtVim", import = "ftvim.plugins" },
-    -- import/override with your plugins
-    { import = "plugins" },
-  },
-  checker = { enabled = true, notify = true }, -- automatically check for plugin updates
-}
+require("lazy").setup({
+        spec = {
+                -- add FtVim and import its plugins
+                {
+                        "FtVim/FtVim",
+                        lazy = false,
+                        branch = "main",
+                        import = "ftvim.plugins",
+                        config = function()
+                                require("options")
+                        end,
+                },
+                -- import/override with your plugins
+                { import = "plugins" },
+        },
+        checker = { enabled = true, notify = true }, -- automatically check for plugin updates
+})
 
-require "config.toggleterm"
+vim.cmd("colorscheme " .. ftvim.colorscheme)
 
--- <leader>uc for select colorschemes
-require "settings.theme"
+require("ftvim.utils.modules").learn_to_move()
+
+vim.schedule(function()
+        require("keymaps")
+end)
+
+vim.api.nvim_create_autocmd("VimEnter", {
+	callback = function()
+		vim.cmd("LspStart")
+	end,
+})
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,11 +1,3 @@
-return {
-    {
-        "EdenEast/nightfox.nvim",
-    },
-    {
-        "dgox16/oldworld.nvim",
-        lazy = false,
-        priority = 1000,
-        config = true,
-    },
-}
+if true then
+	return {}
+end
